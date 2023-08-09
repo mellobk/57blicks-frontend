9246ed4124e6c9f88df0b9a36af75715fa28d5f3
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-08-08 19:29:48 -0500

    build: upgrade dependencies

--- src/shared/ErrorBoundary/ErrorBoundary.tsx
+++ src/shared/ErrorBoundary/ErrorBoundary.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ComponentType, ReactNode } from "react";
+import { ComponentType, ReactNode } from 'react';
 import {
   ErrorBoundary as Boundary,
   ErrorBoundaryPropsWithFallback,
-} from "react-error-boundary";
+} from 'react-error-boundary';
 
-import { ErrorPageStrategy } from "shared/Result";
+import { ErrorPageStrategy } from 'shared/Result';
 
 export interface FallbackProps<ErrorType> {
   error: ErrorType;
@@ -14,13 +14,13 @@ export interface FallbackProps<ErrorType> {
 export type ErrorFallback<ErrorType> = ComponentType<FallbackProps<ErrorType>>;
 
 interface ErrorBoundaryProps<ErrorType> {
-  onResetKeysChange?: ErrorBoundaryPropsWithFallback["onResetKeysChange"];
+  onResetKeysChange?: ErrorBoundaryPropsWithFallback['onReset'];
   onReset?: () => void;
   onError?: (
     error: Error,
     info: {
       componentStack: string;
-    }
+    },
   ) => void;
   fallback?: ErrorFallback<ErrorType> | React.ReactElement<any, any>;
   resetKeys?: Array<any>;
