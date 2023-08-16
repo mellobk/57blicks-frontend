e19f216425720feaf62fcac91fe092cdd6b52d83
Samir Fragozo <samirfragozob@gmail.com>
2023-08-16 15:18:53 -0500

    Minor fix

--- src/components/layout/Login/Login.tsx
+++ src/components/layout/Login/Login.tsx
@@ -1,6 +1,5 @@
 
 import "./Login.css";
-import Avatar from "../../ui/Avatar/Avatar.tsx";
 
 type Props = {
 	children?: React.ReactNode;
@@ -10,7 +9,6 @@ export const LoginLayout: React.FC<Props> = ({ children }: Props) => {
 	return (
 		<div className="flex justify-between bg-cover bg-center w-screen h-screen box-border p-6 overflow-auto Login-layout-container"
 		data-testid="login-layout-container">
-			<Avatar />
 			<div className="flex flex-col justify-center items-center gap-6 w-56 p-6 rounded-lg bg-white" style={{width: '21.875rem'}}>
 				{children}
 			</div>
