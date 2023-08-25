94e0124f5db4fd0bf1f5ddfa81cdb9e80b222c5b
Samir Fragozo <samirfragozob@gmail.com>
2023-08-25 03:36:04 -0500

    Inter font

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/tailwind.css";
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
-import 'primeicons/primeicons.css';
+import "primeicons/primeicons.css";
+import "unfonts.css";
+import "./styles/tailwind.css";
 
 const rootElement = document.querySelector("#root") as Element;
 if (!rootElement.innerHTML) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,27 @@
 import { defineConfig } from "vitest/config";
+import Unfonts from "unplugin-fonts/vite";
 import react from "@vitejs/plugin-react-swc";
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		Unfonts({
+			google: {
+				families: ["Inter"],
+			},
+		}),
+	],
 	server: {
 		host: true,
 		strictPort: true,
 	},
 	resolve: {
 		alias: {
-		  '@': path.resolve(__dirname, 'src'), // Adjust the path accordingly
+			"@": path.resolve(__dirname, "src"), // Adjust the path accordingly
 		},
-	  },
+	},
 	test: {
 		environment: "jsdom",
 		setupFiles: ["./vitest.setup.ts"],
