5b7e630c9a269efcfa969363fe6721eb1e54e852
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-08-28 01:59:33 -0500

    feat(config): initialize global configuration directory

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export const APP_ENVIRONMENT = import.meta.env.VITE_APP_ENVIRONMENT;
+
+export const COGNITO_USER_POOL_ID = import.meta.env.VITE_COGNITO_USER_POOL_ID;
+
+export const COGNITO_CLIENT_ID = import.meta.env.VITE_COGNITO_CLIENT_ID;
