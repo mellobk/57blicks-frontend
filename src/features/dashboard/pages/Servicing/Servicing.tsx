609d079598481dfb59fa22014432ebab5f9ba99c
Samir Fragozo <samirfragozob@gmail.com>
2023-09-16 23:11:48 -0500

    Opportunities - pages removed

--- src/features/dashboard/pages/Servicing/Servicing.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from "react";
-
-export const Servicing: FC = () => {
-	return (
-		<div className="flex flex-col items-center gap-3 h-full">
-			<h1>Servicing</h1>
-		</div>
-	);
-};
