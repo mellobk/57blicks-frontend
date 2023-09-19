52a525d121c2660ca87ebadb4cc6142d86373288
Samir Fragozo <samirfragozob@gmail.com>
2023-09-19 01:13:50 -0500

    Opportunities - name format

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -6,3 +6,9 @@ export const moneyFormat = (value: number) => {
 
 	return USDollar.format(value);
 };
+
+export const nameFormat = (name: string) => {
+	return name.replace(/\b[a-z]/g, function (match) {
+		return match.toUpperCase();
+	});
+};
