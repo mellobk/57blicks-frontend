86c0cba0f486e59e813092b32c2884e043281de0
SMALBACH\smalb <smalbach24@gmail.com>
2023-09-23 15:04:49 -0500

    feat: add format date

---
+++ src/utils/date-format.ts
@@ -0,0 +1,33 @@
+export function formatDate(inputDate: string): string {
+	const months: Array<string> = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	];
+
+	const parts: Array<string> = inputDate.split("-");
+	if (parts.length !== 3) {
+		return "Invalid date format";
+	}
+
+	const year: number = Number.parseInt(parts[0]);
+	const monthIndex: number = Number.parseInt(parts[1]) - 1;
+	const day: number = Number.parseInt(parts[2]);
+
+	if (isNaN(year) || isNaN(monthIndex) || isNaN(day)) {
+		return "Invalid date values";
+	}
+
+	const month: string = months[monthIndex];
+
+	return `${month} ${day}, ${year}`;
+}
