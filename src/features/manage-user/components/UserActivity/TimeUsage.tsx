ec8a5f35dc2c4e981ab843f13c94f4d19135ab0e
SMALBACH\smalb <smalbach24@gmail.com>
2023-09-23 15:03:51 -0500

    feat: add component time usage

---
+++ src/features/manage-user/components/UserActivity/TimeUsage.tsx
@@ -0,0 +1,22 @@
+import { Icon } from "@/components/ui/Icon";
+import React from "react";
+import { UserActivity } from "../../types/logs";
+import { formatDate } from "@/utils/date-format";
+
+interface TimeUsageProps {}
+
+const TimeUsage: React.FC<TimeUsageProps> = () => {
+	return (
+		<div className="relative h-40">
+			<div className="absolute w-40 h-7  bg-green-800 pt-1 pb-1 pl-3 pr-3 rounded-[15px] flex  text-xs text-green-500 font-bold align-middle ">
+				<div className="pr-2 align-middle pt-0.5  ">
+					<Icon name="hamburger" width="12" color="#00BA35" />
+				</div>
+				Time usage
+			</div>
+			<div className="absolute top-16 text-2xl font-semibold z-40 "></div>
+		</div>
+	);
+};
+
+export default TimeUsage;
