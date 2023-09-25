32e71a18796706a006c56aeb27db9f209730611d
Samir Fragozo <samirfragozob@gmail.com>
2023-09-24 19:04:10 -0500

    Select refactored

--- src/utils/class-names.ts
+++ src/utils/class-names.ts
@@ -1,5 +1,5 @@
 export const inputClassName = (error?: string) => {
-	return `placeholder-gray-400 focus:outline-none ${
+	return `placeholder-gray-400 focus:outline-none border-none shadow-none ${
     error ? "text-red-ERROR bg-gray-100" : "bg-gray-200"
   } font-inter text-[13px] text-primary-500 leading-4 tracking-[-0.65px] flex w-full h-10 p-4 items-center self-stretch rounded-md`;
 };
