582fc1c37603e66f9e8bf5d22ddfcbb894a0de53
SMALBACH\smalb <smalbach24@gmail.com>
2023-09-26 23:00:59 -0500

    feat: add modal date

--- src/features/manage-user/components/UserActivity/UserActivity.tsx
+++ src/features/manage-user/components/UserActivity/UserActivity.tsx
@@ -1,84 +1,85 @@
 /* eslint-disable no-use-before-define */
 import { useEffect, useState } from "react";
 
+import { CalendarModal } from "@/components/ui/CalendarModal/CalendarModal";
 import LastLogin from "./LastLogin";
 import ManageLogService from "../../api/logs";
-import { Modal } from "@/components/ui/Modal/Modal";
 import NumberOfLogin from "./NumberOfLogin";
-import { Tabs } from "@/features/servicing/component/Tabs";
 import TimeUsage from "./TimeUsage";
 import type { User } from "../../types/api";
 import { useQuery } from "@tanstack/react-query";
-import { userTabs } from "@/features/servicing/utils/tabs";
 
 interface UserActivityProps {
 	user: User;
-	setUser: (user: User | null) => void;
 }
-const UserActivity: React.FC<UserActivityProps> = ({ user, setUser }) => {
-	const [actualTabData, setActualTabData] = useState<string>("activity");
-	const [openModal, setOpenModal] = useState<boolean>(true);
+const UserActivity: React.FC<UserActivityProps> = ({ user }) => {
+	const [showModal, setShowModal] = useState(false);
+	const [date, setDate] = useState<Date>(new Date());
 
 	const userActivity = useQuery(
 		["logs-user-activity"],
 		() => {
-			return ManageLogService.getUserActivity(user.id || "", new Date());
+			return ManageLogService.getUserActivity(user.id || "", date);
 		},
 		{ enabled: true, staleTime: 1000 * 60 }
 	);
 
-	const closeModal = (): void => {
-		setOpenModal(false);
-		setUser(null);
-	};
-
-	const tabHandlerData = (value: string): void => {
-		setActualTabData(value);
-	};
+	useEffect(() => {
+		void getUserActivity();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [user]);
 
 	useEffect(() => {
-		setOpenModal(true);
 		void getUserActivity();
-	}, [user]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [date]);
 
 	const getUserActivity = async (): Promise<void> => {
 		await userActivity.refetch();
 	};
 
-	return (
-		<div>
-			<Modal
-				width="90%"
-				minHeight="80vh"
-				visible={openModal}
-				onHide={closeModal}
-				title={user.firstName}
-			>
-				<div className="absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2   ">
-					<div className="">
-						<Tabs
-							tabs={userTabs}
-							actualTab={actualTabData}
-							onClick={tabHandlerData}
-						/>
-					</div>
-				</div>
+	const handleSetDate = async (
+		date: string | Date | Array<Date> | null
+	): Promise<void> => {
+		setDate(date as Date);
 
-				<div className="flex mb-4 gap-10">
-					<div className="lg:w-1/2 md:w-full p-5 shadow-lg rounded-lg h-1/2 ">
-						{userActivity.data && <LastLogin data={userActivity.data} />}
-					</div>
-					<div className="lg:w-1/2 md:w-full p-5 shadow-lg rounded-lg h-1/2">
-						{userActivity.data && <NumberOfLogin data={userActivity.data} />}
-					</div>
+		await userActivity.refetch();
+	};
+
+	return (
+		<>
+			<div
+				className="absolute top-10 right-10 cursor-pointer	 transform -translate-x-1/2 -translate-y-1/2 bg-gold-100 pt-1 pb-1.5 pl-4 pr-4 text-gold-500 text-sm font-semibold rounded-3xl hover:bg-gold-350"
+				onClick={() => {
+					setShowModal(true);
+				}}
+			>
+				Select Dates
+			</div>
+
+			<div className="flex mb-4 gap-10">
+				<div className="lg:w-1/2 md:w-full p-5 shadow-lg rounded-lg h-1/2 ">
+					{userActivity.data && <LastLogin data={userActivity.data} />}
 				</div>
-				<div className="flex mb-4">
-					<div className="w-full p-5 shadow-lg rounded-lg h-1/2">
-						<TimeUsage />
-					</div>
+				<div className="lg:w-1/2 md:w-full p-5 shadow-lg rounded-lg h-1/2">
+					{userActivity.data && <NumberOfLogin data={userActivity.data} />}
 				</div>
-			</Modal>
-		</div>
+			</div>
+			<div className="flex mb-4">
+				<div className="w-full p-5 shadow-lg rounded-lg h-1/2">
+					{userActivity.data && (
+						<TimeUsage data={userActivity.data} date={date} />
+					)}
+				</div>
+			</div>
+			<CalendarModal
+				show={showModal}
+				setShow={setShowModal}
+				date={date}
+				setDate={handleSetDate}
+				closeOnSelect={true}
+			/>
+		</>
 	);
 };
 
