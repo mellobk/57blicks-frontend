e9d74b1363c84873c854560e7c775d477cfbe222
SMALBACH\smalb <smalbach24@gmail.com>
2023-09-26 22:56:12 -0500

    fix: change order providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "@tanstack/router";
-import type { FunctionComponent } from "./types";
-import { router } from "./routes";
-import { PrimeReactProvider } from "primereact/api";
-import { ErrorBoundary } from "react-error-boundary";
+
 import { AuthProvider } from "./providers/AuthContextProvider";
+import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "@/components/ui/ErrorFallback";
-import { TanStackRouterDevelopmentTools } from "@/utils/development-tools/TanStackRouterDevelopmentTools";
+import type { FunctionComponent } from "./types";
+import { PrimeReactProvider } from "primereact/api";
 import { ReactQueryDevelopmentTools } from "@/utils/development-tools/ReactQueryDevelopmentTools";
+import { RouterProvider } from "@tanstack/router";
+import { TanStackRouterDevelopmentTools } from "@/utils/development-tools/TanStackRouterDevelopmentTools";
+import { router } from "./routes";
 const queryClient = new QueryClient();
 
 const App = (): FunctionComponent => {
 	return (
-		<AuthProvider>
-			<PrimeReactProvider>
-				<ErrorBoundary fallback={<ErrorFallback />}>
-					<QueryClientProvider client={queryClient}>
+		<PrimeReactProvider>
+			<ErrorBoundary fallback={<ErrorFallback />}>
+				<QueryClientProvider client={queryClient}>
+					<AuthProvider>
 						<RouterProvider router={router} />
 						<TanStackRouterDevelopmentTools
 							router={router}
@@ -23,10 +24,10 @@ const App = (): FunctionComponent => {
 							position="bottom-right"
 						/>
 						<ReactQueryDevelopmentTools initialIsOpen={false} />
-					</QueryClientProvider>
-				</ErrorBoundary>
-			</PrimeReactProvider>
-		</AuthProvider>
+					</AuthProvider>
+				</QueryClientProvider>
+			</ErrorBoundary>
+		</PrimeReactProvider>
 	);
 };
 
