4047da01f12d10d0e7234245aca5986a79351c61
SMALBACH\smalb <smalbach24@gmail.com>
2023-09-26 23:02:03 -0500

    feat: add log types

--- src/features/manage-user/types/logs.ts
+++ src/features/manage-user/types/logs.ts
@@ -1,3 +1,4 @@
+import { accessToken } from "./../../../utils/constant";
 export interface NumberOfLogin {
 	logins: string;
 	date: string;
@@ -16,4 +17,24 @@ export interface UserActivity {
 	userActivity: Array<unknown>;
 	numberOfLogins: Array<NumberOfLogin>;
 	lastLogin: LastLogin;
+}
+
+export enum LogTable {
+	USER = "user",
+}
+
+export enum LogAction {
+	USER_LOGIN = "user:login",
+	USER_LOGOUT = "user:logout",
+}
+
+export interface Log {
+	event: LogAction;
+	entity: LogTable;
+}
+
+export interface LogLoign {
+	event: LogAction;
+	entity: LogTable;
+	accessToken: string;
 }
