68969fb4fc02d605b186aa777d0d7dd7bf2e532e
SMALBACH\smalb <smalbach24@gmail.com>
2023-09-26 23:35:50 -0500

    fix: change interceptor to all request

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -1,22 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable unicorn/prevent-abbreviations */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { accessToken } from "./constant.ts";
 import axios from "axios";
 import { getLocalStorage } from "./local-storage.ts";
-import { accessToken } from "./constant.ts";
-import { signOut } from "@/lib/cognito";
 import sharedObject from "@/config/api-config";
+import { signOut } from "@/lib/cognito";
 
 export const authApiClient = axios.create({
 	baseURL: sharedObject.api.ENDPOINT,
-	headers: {
-		"Content-type": "application/json",
-		Authorization: `Bearer ${getLocalStorage(accessToken)}`,
-	},
 });
+
+authApiClient.interceptors.request.use(
+	(config: any) => {
+		config.headers.Authorization = `Bearer  ${getLocalStorage(accessToken)}`;
+		config.headers["Content-type"] = "application/json";
+
+		return config;
+	},
+	(err: any) => {
+		return Promise.reject(err);
+	}
+);
 
 authApiClient.interceptors.response.use(
 	(response) => {
