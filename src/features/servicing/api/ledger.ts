85fc8a2c4499dc106acaedc1338b1bf8f4891a8a
Samir Fragozo <samirfragozob@gmail.com>
2023-10-11 16:50:10 -0300

    Minor fix

--- src/features/servicing/api/ledger.ts
+++ src/features/servicing/api/ledger.ts
@@ -1,12 +1,10 @@
 import type { LedgerFormValues, Ledgers } from "../component/Ledger/types";
 
-import type { Ledger } from "../../../features/servicing/component/Ledger/types";
+import type { Ledger } from "@/features/servicing/component/Ledger/types";
 import { authApiClient } from "@/utils/api-client";
 import { ledgers } from "./backend-end-points";
 
-const getLedgerByLoanId = async (
-	id: string
-): Promise<Array<Ledgers> | null> => {
+const getLedgerByLoanId = async (id: string): Promise<Array<Ledgers> | null> => {
 	try {
 		const response = await authApiClient.get<Array<Ledger>>(
 			`${ledgers()}/get-by-loan/${id}?take=1000`
@@ -16,17 +14,18 @@ const getLedgerByLoanId = async (
 	} catch {
 		/* empty */
 	}
+
 	return null;
 };
 
 const createLedger = async (data: LedgerFormValues) => {
 	try {
-		const response = await authApiClient.post(ledgers(), data);
-		return response;
+		return await authApiClient.post(ledgers(), data);
 	} catch (error) {
 		return error;
 	}
 };
+
 const ManageLedgerService = {
 	createLedger,
 	getLedgerByLoanId,
