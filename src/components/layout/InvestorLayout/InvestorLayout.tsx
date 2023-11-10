9cf665c42d2cc49f4816372d020234fd4a72f316
Pagony <pagomezo@unal.edu.co>
2023-11-10 02:15:11 -0500

    Profile men for the investor portal in order to have the logoff functionality

--- src/components/layout/InvestorLayout/InvestorLayout.tsx
+++ src/components/layout/InvestorLayout/InvestorLayout.tsx
@@ -3,22 +3,27 @@ import "@/assets/images/png/LogoGold_2x.png";
 import "./InvestorLayout.css";
 import { useQuery } from "@tanstack/react-query";
 import userStore from "@/stores/user-store.ts";
-import { sendToLocalStorage } from "@/utils/local-storage";
+import { getLocalStorage, sendToLocalStorage } from "@/utils/local-storage";
 import { userName } from "@/utils/constant";
 import UserService from "../../../api/user.ts";
-import { Link } from "@tanstack/router";
+import { Link, useNavigate } from "@tanstack/router";
 import { InvestorRoutes } from "@/features/investor/routes/InvestorRouter.tsx";
 import LogoGold from "@/assets/images/png/LogoGold.png";
 import { Icon } from "@/components/ui/Icon";
+import { Avatar } from "@/components/ui/Avatar";
+import { LogOff } from "@/features/admin/components/profile/component/LogOff/LogOff";
 
 type Props = {
 	children?: ReactNode;
 };
 
 export const InvestorLayout: FC<Props> = ({ children }) => {
+	const navigate = useNavigate();
 	const userLoggedInfo = userStore((state) => state.setLoggedUserInfo);
 	const [openModalNotification, setOpenModalNotification] = useState<boolean>();
 	const [notificationsCount] = useState<number>();
+	const localUserName = getLocalStorage(userName);
+	const [openModalUser, setOpenModalUser] = useState<boolean>();
 
 	const userLoggedQuery = useQuery(
 		["user-logged-query"],
@@ -31,7 +36,14 @@ export const InvestorLayout: FC<Props> = ({ children }) => {
 	const handleOpenModalNotification = (): void => {
 		setOpenModalNotification(!openModalNotification);
 	};
+
+	const handleOpenModal = (): void => {
+		setOpenModalUser(!openModalUser);
+	};
 	
+	const navigateToProfile = (): void => {
+		void navigate({ to: `/profile` });
+	};
 
 	useEffect(() => {
 		if (userLoggedQuery.data) {
@@ -88,9 +100,44 @@ export const InvestorLayout: FC<Props> = ({ children }) => {
 							)}
 							<Icon name="notification" color={"#dcdfe0"} width="25" />{" "}
 					</div>
+					<div onClick={handleOpenModal} className="flex cursor-pointer">
+						<Avatar name={localUserName} />
+					</div>
 				</div>
 			</div>
 			<div className="flex m-2 h-screen overflow-y-auto">{children}</div>
+			{openModalUser && (
+				<>
+					<div
+						className="w-full h-full absolute bg-gray-400 opacity-30 z-30"
+						onClick={handleOpenModal}
+					></div>
+					<div
+						className="absolute rounded-[13px] bg-white flex flex-col gap-2 "
+						style={{
+							width: "15%",
+							right: "50px",
+							top: "55px",
+							padding: "10px",
+							zIndex: "100",
+						}}
+					>
+						<div className="border-b border-gray-200 pb-2 flex gap-2 items-center">
+							<Avatar name={localUserName} />
+							{localUserName}
+						</div>
+
+						<div
+							className=" flex gap-3 items-center p-1 cursor-pointer"
+							onClick={navigateToProfile}
+						>
+							<Icon name="passwordProfile" color="black" width="20" />
+							My profile
+						</div>
+						<LogOff />
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
