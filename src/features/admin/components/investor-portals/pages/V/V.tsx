7a04547345f8b0050bc968a45a43b3995533a0c4
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-12-01 03:49:30 -0500

    feat: rename First Capital Trusts to DKC Lending V

--- src/features/admin/components/investor-portals/pages/V/V.tsx
+++ src/features/admin/components/investor-portals/pages/V/V.tsx
@@ -1,6 +1,4 @@
 import type { FC } from "react";
 import { Page } from "@/features/admin/components/investor-portals/component/Page/Page";
 
-export const FirstCapital: FC = () => (
-	<Page actualTab="First Capital Trusts LLC" id="First Capital Trust" />
-);
+export const V: FC = () => <Page actualTab="DKC Lending V" />;
--- src/features/admin/components/investor-portals/utils/tabs.ts
+++ src/features/admin/components/investor-portals/utils/tabs.ts
@@ -3,5 +3,5 @@ export const investorPortalsTabs = [
 	{ label: "DKC Lending FL", routeTo: "investor-portals/dkc-fl" },
 	{ label: "DKC Lending IV", routeTo: "investor-portals/dkc-iv" },
 	{ label: "DKC Lending CL", routeTo: "investor-portals/dkc-cl" },
-	{ label: "First Capital Trusts LLC", routeTo: "investor-portals/first-capital" },
+	{ label: "DKC Lending V", routeTo: "investor-portals/dkc-v" },
 ];
