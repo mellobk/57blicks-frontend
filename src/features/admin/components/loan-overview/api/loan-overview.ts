f759b10538a4ab9b0d6b6eaa49c022365f6f14ae
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-11-30 21:59:20 -0500

    feat(loan-overview): add update trust unallocated api endpoint

--- src/features/admin/components/loan-overview/api/loan-overview.ts
+++ src/features/admin/components/loan-overview/api/loan-overview.ts
@@ -1,7 +1,17 @@
 import { authApiClient } from "@/utils/api-client";
-import type { ILoanOverview } from "../types/fields";
+import type {
+	ILoanOverview,
+	UpdateParticipationBreakdownDto,
+} from "../types/fields";
 
 export const getLoanOverview = async (): Promise<ILoanOverview> => {
 	const response = await authApiClient.get<ILoanOverview>("/loans/overview");
 	return response.data;
+};
+
+export const updateTrustUnallocated = async (
+	id: string,
+	body: UpdateParticipationBreakdownDto
+): Promise<void> => {
+	await authApiClient.put(`/participation-breakdowns/${id}`, body);
 };
