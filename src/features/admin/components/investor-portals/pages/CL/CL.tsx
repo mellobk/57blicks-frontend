65a3768d6d8688aee42479624b9e839c0207fc22
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-12-18 17:37:02 -0500

    fix: rename lender DKC Lending LC to DKC Lending CL

--- src/features/admin/components/investor-portals/pages/CL/CL.tsx
+++ src/features/admin/components/investor-portals/pages/CL/CL.tsx
@@ -2,5 +2,5 @@ import type { FC } from "react";
 import { Page } from "@/features/admin/components/investor-portals/component/Page/Page";
 
 export const CL: FC = () => (
-	<Page actualTab="DKC Lending CL" id="DKC Lending LC" />
+	<Page actualTab="DKC Lending CL" id="DKC Lending CL" />
 );
--- src/types/api/lender.ts
+++ src/types/api/lender.ts
@@ -15,7 +15,7 @@ export enum LenderNameType {
 	DKC_LENDING_LLC = "DKC Lending LLC",
 	DKC_LENDING_FL = "DKC Lending FL",
 	DKC_LENDING_IV = "DKC Lending IV",
-	DKC_LENDING_LC = "DKC Lending LC",
+	DKC_LENDING_CL = "DKC Lending CL",
 	DKC_LENDING_V = "DKC Lending V",
 	YIELD_SPREAD = "Yield Spread",
 	DKC_SERVICING_FEE_INCOME = "DKC Servicing Fee Income",
