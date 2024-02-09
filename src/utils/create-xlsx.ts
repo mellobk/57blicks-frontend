e23f281e35e62cbc82a837b858bfc38723e6b6f5
brayan manrique <brayan.bonilla@revstarconsulting.com>
2024-02-09 17:27:52 -0500

    addin new library to create excel

--- src/utils/create-xlsx.ts
+++ src/utils/create-xlsx.ts
@@ -5,40 +5,25 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import * as XLSX from "xlsx";
+import ExcelJS from "exceljs";
 import { saveAs } from "file-saver";
 
-export const downloadXLSX = (data: Array<any>, fileName: string) => {
-	// Preprocess data to remove unwanted indices or modify it as needed
-	const preprocessedData = data.map((item) => {
-		// Assuming 'item' is an object, you can remove or modify it here
-		// For example, delete item.unwantedProperty;
-		return item;
+export const downloadXLSX = async (data: Array<any>, fileName: string) => {
+	// Create a new workbook and a worksheet
+	const workbook = new ExcelJS.Workbook();
+	const worksheet = workbook.addWorksheet("My Sheet");
+
+	// Add your data to the worksheet
+	data.forEach((row) => {
+		worksheet.addRow(row);
 	});
 
-	// Continue with your conversion after preprocessing
-	const worksheet = XLSX.utils.json_to_sheet(preprocessedData);
-	const workbook = XLSX.utils.book_new();
+	// Write the workbook to a buffer
+	const buffer = await workbook.xlsx.writeBuffer();
 
-	delete worksheet["A1"];
-	delete worksheet["B1"];
-	delete worksheet["C1"];
-	delete worksheet["D1"];
-	delete worksheet["E1"];
-	delete worksheet["F1"];
-	delete worksheet["G1"];
-	delete worksheet["H1"];
-
-	XLSX.utils.book_append_sheet(workbook, worksheet, "sheet1");
-
-	const about = XLSX.write(workbook, { bookType: "xlsx", type: "binary" });
-	const buf = new ArrayBuffer(about.length);
-	const view = new Uint8Array(buf);
-
-	for (let index = 0; index < about.length; index++) {
-		view[index] = about.charCodeAt(index) & 0xff;
-	}
-
-	// Assuming 'saveAs' is defined elsewhere or you're using a library like FileSaver.js
-	saveAs(new Blob([buf], { type: "application/octet-stream" }), fileName);
+	// Create a Blob and use FileSaver to save the file
+	const blob = new Blob([buffer], {
+		type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+	});
+	saveAs(blob, fileName);
 };
